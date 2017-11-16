0c16f24adf61b7026f3f7ba82fbab23fab445c57
TheAtomicGoose <jesse27999@gmail.com>
2017-11-15 20:01:20 -0500

    location permissions

--- app/src/main/java/com/jesseevers/cheers/MapsActivity.java
+++ app/src/main/java/com/jesseevers/cheers/MapsActivity.java
@@ -1,16 +1,29 @@
 package com.jesseevers.cheers;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    /**
+     * Request code location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
     private GoogleMap mMap;
 
@@ -22,6 +35,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
 
@@ -38,9 +65,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.setOnMyLocationButtonClickListener(this);
+//        mMap.setOnMyLocationClickListener(this);
+        enableMyLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                // IF request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                } else {
+                    mPermissionDenied = true;
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Locations permission was not granted, show error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 }
+
---
+++ app/src/main/java/com/jesseevers/cheers/PermissionUtils.java
@@ -0,0 +1,70 @@
+package com.jesseevers.cheers;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+/**
+ * Created by jesse on 11/13/17.
+ */
+
+public class PermissionUtils {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    public static void requestPermission(FragmentActivity activity, int requestCode) {
+        ActivityCompat.requestPermissions(activity,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                requestCode);
+    }
+
+    /**
+     * A dialog that displays a permission denied message.
+     */
+    public static class PermissionDeniedDialog extends DialogFragment {
+
+        private static final String ARGUMENT_FINISH_ACTIVITY = "finish";
+
+        private boolean mFinishActivity = false;
+
+        /**
+         * Creates a new instance of this dialog and optionally finishes the calling Activity when
+         * the 'Ok' button is clicked.
+         */
+        public static PermissionDeniedDialog newInstance(boolean finishActivity) {
+            Bundle arguments = new Bundle();
+            arguments.putBoolean(ARGUMENT_FINISH_ACTIVITY, finishActivity);
+
+            PermissionDeniedDialog dialog = new PermissionDeniedDialog();
+            dialog.setArguments(arguments);
+            return dialog;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            mFinishActivity = getArguments().getBoolean(ARGUMENT_FINISH_ACTIVITY);
+
+            return new AlertDialog.Builder(getActivity())
+                    .setMessage(R.string.location_permission_denied)
+                    .setPositiveButton(android.R.string.ok, null)
+                    .create();
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            super.onDismiss(dialog);
+            if (mFinishActivity) {
+                Toast.makeText(getActivity(), R.string.permission_required_toast,
+                        Toast.LENGTH_SHORT).show();
+                getActivity().finish();
+            }
+        }
+    }
+
+}
